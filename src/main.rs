080c270c00686676cb95b273ce6fb12407524744
Kazurus <akazurus@gmail.com>
2023-11-28 21:30:04 +0300

    chore: setup embed base settings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+use rtt_target::{rtt_init_print, rprintln};
+
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("Hello World 1");
+    loop {}
 }
