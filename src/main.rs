b9b6fc7ca9a96424eaa57b1aa57e110d0309559c
Kazurus <akazurus@gmail.com>
2023-12-22 00:16:16 +0300

    feat: add mvp version for sonar logic

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,20 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
-// use core::fmt::Write;
+use core::borrow::BorrowMut;
+use core::cell::RefCell;
 
 use defmt::{info, println};
 use defmt_rtt as _;
 
 use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 // use cortex_m_rt::entry;
-use embassy_stm32::peripherals::{DMA2_CH2, DMA2_CH7, USART1, self};
+use embassy_stm32::peripherals::{self, DMA2_CH2, DMA2_CH7, PC8, PC9, USART1};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_sync::pubsub::{PubSubChannel, PubSubBehavior};
+use embassy_sync::mutex::Mutex;
+use embassy_sync::pubsub::{PubSubBehavior, PubSubChannel};
+use embassy_time::{Instant, Timer};
 use panic_probe as _;
 // use panic_halt as _;
 use embassy_stm32::usart::{Config, Uart};
@@ -33,6 +37,8 @@ enum DirectionCommand {
 }
 
 static SHARED: PubSubChannel<ThreadModeRawMutex, DirectionCommand, 1, 2, 2> = PubSubChannel::new();
+static CURRENT_DIRECTION: Mutex<ThreadModeRawMutex, RefCell<DirectionCommand>> =
+    Mutex::new(RefCell::new(DirectionCommand::Stop));
 
 bind_interrupts!(struct Irqs {
     USART1 => usart::InterruptHandler<USART1>;
@@ -107,7 +113,64 @@ async fn handle_direction_command(mut chassis: Chassis<peripherals::PA0, periphe
             DirectionCommand::Left => chassis.left(),
             DirectionCommand::Right => chassis.right(),
             DirectionCommand::Stop => chassis.stop(),
-            _ => info!("Receive unknown command"), 
+            _ => info!("Receive unknown command"),
+        }
+    }
+}
+
+#[embassy_executor::task]
+async fn handle_distance_update(
+    mut sonar_trig: Output<'static, PC8>,
+    sonar_echo: Input<'static, PC9>,
+) {
+    loop {
+        sonar_trig.set_low();
+        Timer::after_micros(5).await;
+
+        sonar_trig.set_high();
+        Timer::after_micros(10).await;
+
+        while !(sonar_echo.is_high()) {}
+
+        let inst = Instant::now();
+
+        while !(sonar_echo.is_low()) {}
+
+        let duration = Instant::checked_duration_since(&Instant::now(), inst).unwrap();
+        let distance_cm = duration.as_micros() / 2 / 29;
+
+        info!("Distance cm: {:?}", distance_cm);
+
+        let direction_lock = CURRENT_DIRECTION.lock().await;
+        let direction = direction_lock.borrow().clone();
+        drop(direction_lock);
+
+        let new_direction = match direction {
+            DirectionCommand::Forward if distance_cm < 50 => {
+                info!("Should stop");
+                DirectionCommand::Stop
+            }
+            DirectionCommand::Stop if distance_cm < 50 => {
+                info!("Should rotate");
+                if distance_cm % 2 == 0 {
+                    DirectionCommand::Left
+                } else {
+                    DirectionCommand::Right
+                }
+            }
+            DirectionCommand::Left | DirectionCommand::Right if distance_cm > 50 => {
+                info!("Should go forward");
+                DirectionCommand::Forward
+            }
+            _ => DirectionCommand::Unknown,
+        };
+
+        match new_direction {
+            DirectionCommand::Unknown => Timer::after_secs(1).await,
+            new_dir => {
+                SHARED.publish_immediate(new_dir);
+                Timer::after_secs(3).await;
+            }
         }
     }
 }
@@ -125,6 +188,9 @@ async fn main(spawner: Spawner) -> ! {
         // Uart::new(p.USART6, p.PC7, p.PC6, Irqs, p.DMA1_CH6, p.DMA1_CH5, config).unwrap();
         Uart::new(p.USART1, p.PB7, p.PB6, Irqs, p.DMA2_CH7, p.DMA2_CH2, config).unwrap();
 
+    let sonar_trig = Output::new(p.PC8, Level::Low, Speed::Low);
+    let sonar_echo = Input::new(p.PC9, Pull::None);
+
     let wheel_left = WheelPinPair::new(p.PA8, p.PA0);
     let wheel_right = WheelPinPair::new(p.PA7, p.PA1);
     let fwd = WheelDrive::new(wheel_left, wheel_right, Channel::Ch1);
@@ -138,12 +204,17 @@ async fn main(spawner: Spawner) -> ! {
 
     spawner.spawn(handle_direction_command(chassis)).unwrap();
     spawner.spawn(wait_bluetooth_commands(usart)).unwrap();
+    spawner
+        .spawn(handle_distance_update(sonar_trig, sonar_echo))
+        .unwrap();
 
     let mut direction_sub = SHARED.subscriber().unwrap();
     // Application Loop
     loop {
-        
         let direction = direction_sub.next_message_pure().await;
+        let mut direction_lock = CURRENT_DIRECTION.lock().await;
+        direction_lock.borrow_mut().replace(direction.clone());
+        drop(direction_lock);
 
         match direction {
             DirectionCommand::Forward => info!("Command: Forward"),
@@ -151,7 +222,7 @@ async fn main(spawner: Spawner) -> ! {
             DirectionCommand::Left => info!("Command: Left"),
             DirectionCommand::Right => info!("Command: Right"),
             DirectionCommand::Stop => info!("Command: Stop"),
-            _ => info!("Receive unknown command"), 
+            _ => info!("Receive unknown command"),
         }
     }
 }
